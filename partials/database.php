0a1fc4bf145f2fa4f17b9ac34f0800c5816bf42e
gabriele-annese <gannese06@gmail.com>
2022-01-17 18:42:57 +0100

    add Departments Archive

---
+++ partials/database.php
@@ -0,0 +1,13 @@
+<?php 
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'root');
+    define('DB_NAME', 'university');
+
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    
+    if($conn->connect_error){
+        die( 'connection failed' );
+    }
+?>
